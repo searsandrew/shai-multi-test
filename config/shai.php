02b8868688e43add7af0ca4c0875c698c7519e0c
Andrew Sears <searsandrew@gmail.com>
2022-10-26 02:51:20 -0500

    Added config file

---
+++ config/shai.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'version' => '0.1',
+    'logo' => env('APP_LOGO', '/images/shai.svg'),
+    'mark' => env('APP_MARK', '/images/shai.svg'),
+    'card' => env('APP_CARD_LOGO', '/images/shai.svg'),
+    'organization' => env('ORGANIZATION_NAME', 'Shai Donation Platform'),
+    'organization_logo' => env('ORGANIZATION_LOGO', '/images/shai-logo.svg'),
+    'organization_url' => env('ORGANIZATION_URL', 'https://www.github.com/searsandrew/shai'),
+    'organization_sponsor' => env('ORGANIZATION_SPONSOR', 'https://www.patreon.com/mayfifteenth'),
+    'organization_shop' => env('ORGANIZATION_SHOP', FALSE),
+    'organization_description' => env('ORGANIZATION_DESCRIPTION', 'Shai is a donation/giving platform for community churches and organizations that provide seasonal based outreach programs (back-to-school backpacks, Christmas gifts, etc).')
+];
